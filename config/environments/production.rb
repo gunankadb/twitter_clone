c0320c351b0eb3e71c993e83643ecd5f915838fb
Gunanka <gunanka@gmail.com>
2016-05-04 20:41:19 +0530

    adding initialize_on_precompile: false to application.rb => for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,5 +66,4 @@ TwitterClone::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
   config.cache_store = :mem_cache_store
 
-  config.assets.initialize_on_precompile = false
 end
